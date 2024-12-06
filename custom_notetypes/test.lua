3de4ff4112ce70a9c1c4239e21c54ca396cbea95
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-06 14:32:06 -0500

    Update test.lua

--- custom_notetypes/test.lua
+++ custom_notetypes/test.lua
@@ -6,6 +6,9 @@ function onCreate()
 			setPropertyFromGroup('unspawnNotes', i, 'texture', 'HURTNOTE_assets'); --Change texture
 			setPropertyFromGroup('unspawnNotes', i, 'noteSplashDisabled', false);
 			setPropertyFromGroup('unspawnNotes', i, 'animSuffix', '-alt')
+			setPropertyFromGroup('unspawnNotes', i, 'scale.x', 0.60)
+			setPropertyFromGroup('unspawnNotes', i, 'scale.y', 0.60)
+			setPropertyFromGroup('unspawnNotes', i, 'offsetX', 4)
 
 			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == true then --Lets Opponent's instakill notes get ignored
 				setPropertyFromGroup('unspawnNotes', i, 'ignoreNote', false); --Miss has no penalties
